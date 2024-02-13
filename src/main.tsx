dae54917f8783b0156dbfe5e7c4b39a789db9cee
Odegaa <odegaa0202@gmail.com>
2024-02-13 14:10:01 +0500

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import ReactDOM from 'react-dom/client'
-import { App } from './App'
-import 'src/assets/styles/base/_reset.scss'
-import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from 'react-query'
-const queryClient = new QueryClient()
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+import 'src/assets/styles/base/_reset.scss';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<BrowserRouter>
-		<QueryClientProvider client={queryClient}>
-			<App />
-		</QueryClientProvider>
-	</BrowserRouter>
-)
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </BrowserRouter>,
+);
